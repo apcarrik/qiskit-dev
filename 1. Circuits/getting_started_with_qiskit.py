732deba50fce9a2d913528311a88b544ca972501
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-06-11 11:39:11 -0400

    finishing script

--- 1. Circuits/getting_started_with_qiskit.py
+++ 1. Circuits/getting_started_with_qiskit.py
@@ -23,4 +23,33 @@ outputstate = result.get_statevector(circ, decimals=3)
 print(outputstate)
 
 from qiskit.visualization import plot_state_city
-plot_state_city(outputstate) #TODO: Figure out why this isn't working
+plot_state_city(outputstate)
+pyplot.show()
+
+### Unitary Backend
+backend = Aer.get_backend('unitary_simulator')
+job = execute(circ, backend)
+result = job.result()
+
+print(result.get_unitary(circ, decimals=3))
+
+### Open QASM Backend
+# New circuit with classical bits
+meas = QuantumCircuit(3,3)
+meas.barrier(range(3))
+meas.measure(range(3),range(3))
+qc = circ + meas
+qc.draw('mpl')
+pyplot.show()
+
+# Simulate
+backend_sim = Aer.get_backend('qasm_simulator')
+job_sim = execute(qc, backend_sim, shots=1024)
+result_sim = job_sim.result()
+
+counts = result_sim.get_counts(qc)
+print(counts)
+
+from qiskit.visualization import plot_histogram
+plot_histogram(counts)
+pyplot.show()
